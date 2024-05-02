95033d68b966e3e45886d2b2fea70231ef68bea7
ad-abir <abhishek.das0023@gmail.com>
2024-05-03 01:24:11 +0600

    Progress

--- portfolio-react/src/Components/About/About.jsx
+++ portfolio-react/src/Components/About/About.jsx
@@ -20,9 +20,29 @@ const About = () => {
                     <p>My passion for frontend development is not only reflected in my extensive experience but also in the enthusiasm and dedication I bring to each project.</p>
                 </div>
                 <div className="about-skills">
-                    <div className="about-skill"><p></p></div>
+                    <div className="about-skill"><p>HTML & CSS</p><hr style={{width:"50%"}} /></div>
+                    <div className="about-skill"><p>React JS</p><hr style={{width:"70%"}} /></div>
+                    <div className="about-skill"><p>JavaScript</p><hr style={{width:"60%"}} /></div>
+                    <div className="about-skill"><p>Next JS</p><hr style={{width:"50%"}} /></div>
                 </div>
             </div>
+        </div>
+        <div className="about-achievements">
+            <div className="about-achievement">
+                <h1>10+</h1>
+                <p>YEARS OF EXPERIENCE</p>
+            </div>
+            <hr />
+            <div className="about-achievement">
+                <h1>90+</h1>
+                <p>PROJECTS COMPLETED</p>
+            </div>
+            <hr />
+            <div className="about-achievement">
+                <h1>15+</h1>
+                <p>HAPPY CLIENTS</p>
+            </div>
+            <hr />
         </div>
     </div>
   )
