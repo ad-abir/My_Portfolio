5237a9356ca39d10a656adbc3afe938ccfe9e238
ad-abir <abhishek.das0023@gmail.com>
2024-05-03 12:42:32 +0600

    Progress

---
+++ portfolio-react/src/Components/MyWork/MyWork.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './MyWork.css'
+import theme_pattern from '../../assets/theme'
+
+const MyWork = () => {
+  return (
+    <div className='mywork'>
+        <div className="mywork-title">
+            <h1>My latest work</h1>
+            <img src="" alt="" />
+        </div>
+    </div>
+  )
+}
+
+export default MyWork
