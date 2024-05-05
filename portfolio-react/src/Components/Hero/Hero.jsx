b0cfb4b07468ba82f5d692ac1594ff6b5219cc61
ad-abir <abhishek.das0023@gmail.com>
2024-05-06 00:04:57 +0600

    Progress

--- portfolio-react/src/Components/Hero/Hero.jsx
+++ portfolio-react/src/Components/Hero/Hero.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import './Hero.css'
 import profile_img from '../../assets/profile_img.svg'
-import resume from '../../assets/My_Resume.pdf'
+import resume from '../../assets/Abhishek Das_Resume.pdf'
 import AnchorLink from 'react-anchor-link-smooth-scroll'
 
 const Hero = () => {
 
   const downloadResume = () => {
-    const resumeUrl = resume; // Use the imported resume file directly
+    const resumeUrl = resume;
     const link = document.createElement('a');
     link.href = resumeUrl;
-    link.download = 'My_Resume.pdf'; // Set the filename for the downloaded file
+    link.download = 'Abhishek Das_Resume.pdf';
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
@@ -22,8 +22,8 @@ const Hero = () => {
         <h1><span>I'm Abhishek Das,</span> frontend developer based in Bangladesh.</h1>
         <p>I am a frontend developer from Dhaka, Bangladesh with 2 years of experience in multiple companies like WID and GAOTek.</p>
         <div className="hero-action">
-            <div className="hero-connect"><AnchorLink className='anchor-link' offset={50} href='#contact'>Connect with me</AnchorLink></div>
-            <div className="hero-resume">My resume</div>
+            <div className="hero-connect"><AnchorLink className='anchor-link' offset={50} href='#contact'>Connect with Me</AnchorLink></div>
+            <div className="hero-resume" onClick={downloadResume}>Download Resume</div>
         </div>
     </div>
   )
