75e81008ba96bbffa5d562f5384ec8fce447f474
ad-abir <abhishek.das0023@gmail.com>
2024-05-05 16:05:46 +0600

    Progress

--- portfolio-react/src/Components/Hero/Hero.jsx
+++ portfolio-react/src/Components/Hero/Hero.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
 import './Hero.css'
 import profile_img from '../../assets/profile_img.svg'
+import AnchorLink from 'react-anchor-link-smooth-scroll'
 
 const Hero = () => {
   return (
-    <div className='hero'>
+    <div id='home' className='hero'>
         <img src={profile_img} alt="" />
         <h1><span>I'm Abhishek Das,</span> frontend developer based in Bangladesh.</h1>
         <p>I am a frontend developer from Dhaka, Bangladesh with 2 years of experience in multiple companies like WID and GAOTek.</p>
         <div className="hero-action">
-            <div className="hero-connect">Connect with me</div>
+            <div className="hero-connect"><AnchorLink className='anchor-link' offset={50} href='#contact'>Connect with me</AnchorLink></div>
             <div className="hero-resume">My resume</div>
         </div>
     </div>
