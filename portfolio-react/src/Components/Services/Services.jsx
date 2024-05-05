ff2918e319f9a9df273a94a4d9f6298124e34037
ad-abir <abhishek.das0023@gmail.com>
2024-05-05 16:06:54 +0600

    Progress

--- portfolio-react/src/Components/Services/Services.jsx
+++ portfolio-react/src/Components/Services/Services.jsx
@@ -7,7 +7,7 @@ import arrow_icon from '../../assets/arrow_icon.svg'
 const Services = () => {
   return (
     <div className='services'>
-        <div className="services-title">
+        <div id='services' className="services-title">
             <h1>My Services</h1>
             <img src={theme_pattern} alt="" />
         </div>
