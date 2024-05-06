bcaad59ddae1d496ef01d67b64bd3c7235f4d018
ad-abir <abhishek.das0023@gmail.com>
2024-05-07 00:04:58 +0600

    Progress

--- portfolio-react/src/Components/Navbar/Navbar.jsx
+++ portfolio-react/src/Components/Navbar/Navbar.jsx
@@ -3,6 +3,8 @@ import './Navbar.css'
 // import logo from '../../assets/logo.svg'
 import underline from '../../assets/nav_underline.svg'
 import AnchorLink from 'react-anchor-link-smooth-scroll'
+import menu_open from '../../assets/menu_open.svg'
+import menu_close from '../../assets/menu_close.svg'
 
 const Navbar = () => {
 
@@ -11,7 +13,9 @@ const Navbar = () => {
   return (
     <div className='navbar'>
         {/* <img src={logo} alt="" /> */}
+        <img src={menu_open} alt="" className="nav-mob-open" />
         <ul className="nav-menu">
+            <img src={menu_close} alt="" className="nav-mob-close" />
             <li><AnchorLink className='anchor-link' href='#home'><p onClick={()=>setMenu("home")}>Home</p></AnchorLink>{menu==="home"?<img src={underline} alt=''/>:<></>}</li>
             <li><AnchorLink className='anchor-link' offset={50} href='#about'><p onClick={()=>setMenu("about")}>About Me</p></AnchorLink>{menu==="about"?<img src={underline} alt=''/>:<></>}</li>
             <li><AnchorLink className='anchor-link' offset={50} href='#services'><p onClick={()=>setMenu("services")}>Services</p></AnchorLink>{menu==="services"?<img src={underline} alt=''/>:<></>}</li>
