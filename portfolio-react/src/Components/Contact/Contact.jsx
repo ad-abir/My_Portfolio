611b5ee697aa1920096d8d0fe2265fbdf6cc8d7f
ad-abir <abhishek.das0023@gmail.com>
2024-05-07 21:08:53 +0600

    Small changes

--- portfolio-react/src/Components/Contact/Contact.jsx
+++ portfolio-react/src/Components/Contact/Contact.jsx
@@ -33,12 +33,12 @@ const Contact = () => {
   return (
     <div id='contact' className='contact'>
         <div className="contact-title">
-            <h1>Get in touch</h1>
+            <h1>Get in Touch</h1>
             <img src={theme_pattern} alt="" />
         </div>
         <div className="contact-section">
             <div className="contact-left">
-                <h1>Let's talk</h1>
+                <h1>Let's Talk</h1>
                 <p>I'm currently available to take new project, so feel free to send me a message about anything that you want me to work on. You can contact anytime.</p>
                 <div className="contact-details">
                     <div className="contact-detail">
@@ -57,9 +57,9 @@ const Contact = () => {
                 <input type="text" placeholder='Enter your name' name='name' />
                 <label htmlFor="">Your Email</label>
                 <input type="email" placeholder='Enter your email' name='email' />
-                <label htmlFor="">Write your message here</label>
+                <label htmlFor="">Write Your Message Here</label>
                 <textarea name="message" rows="8" placeholder='Enter your message'></textarea>
-                <button type='submit' className="contact-submit">Submit now</button>
+                <button type='submit' className="contact-submit">Submit Now</button>
             </form>
         </div>
     </div>
