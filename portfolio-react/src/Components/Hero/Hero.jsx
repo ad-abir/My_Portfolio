1cdf767ed218b0155f2ebe983e82d8eeccd02e8c
ad-abir <abhishek.das0023@gmail.com>
2024-05-08 16:50:13 +0600

    Updates

--- portfolio-react/src/Components/Hero/Hero.jsx
+++ portfolio-react/src/Components/Hero/Hero.jsx
@@ -3,6 +3,8 @@ import './Hero.css'
 import profile_img from '../../assets/profile_img.svg'
 import resume from '../../assets/Abhishek Das_Resume.pdf'
 import AnchorLink from 'react-anchor-link-smooth-scroll'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLinkedin, faGithub, faFacebook, faInstagram } from '@fortawesome/free-brands-svg-icons'
 
 const Hero = () => {
 
@@ -21,6 +23,20 @@ const Hero = () => {
         <img src={profile_img} alt="" />
         <h1><span>I'm Abhishek Das,</span> Full Stack Developer based in Bangladesh.</h1>
         <p>I am a full stack developer from Dhaka, Bangladesh with 2 years of experience in multiple companies like WID and GAOTek Inc.</p>
+        <div className="social-links">
+          <a href="https://www.linkedin.com/in/ad-abir/" target="_blank" className="social-linkedin">
+            <FontAwesomeIcon icon={faLinkedin} />
+          </a>
+          <a href="https://github.com/ad-abir" target="_blank" className="social-github">
+            <FontAwesomeIcon icon={faGithub} />
+          </a>
+          <a href="https://www.facebook.com/ad.abir01" target="_blank" className="social-facebook">
+            <FontAwesomeIcon icon={faFacebook} />
+          </a>
+          <a href="https://www.instagram.com/ad_abir_/" target="_blank" className="social-instagram">
+            <FontAwesomeIcon icon={faInstagram} />
+          </a>
+        </div>
         <div className="hero-action">
             <div className="hero-connect"><AnchorLink className='anchor-link' offset={50} href='#contact'>Connect with Me</AnchorLink></div>
             <div className="hero-resume" onClick={downloadResume}>Download Resume</div>
